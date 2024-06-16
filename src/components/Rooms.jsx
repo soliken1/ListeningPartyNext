6fe98b8d0898e4ac443b64326b03088b39c4b8dc
Soliken <kennethrex456@gmail.com>
2024-06-16 23:53:09 +0800

    pls bro

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -1,14 +1,15 @@
 "use client";
-
 import React from "react";
 import Link from "next/link";
 import TextFormatter from "../utils/TextFormatter";
 
 const Rooms = ({ data, current }) => {
+  const checkDataIsNull = data || null;
+  const checkCurrentIsNull = current || null;
   const isIdNull = data?.id || "";
   return (
     <Link
-      key={data.id || null}
+      key={checkDataIsNull?.id || ""}
       href={`/rooms/${isIdNull}`}
       className="[&_*]:hover:cursor-pointer max-w-lg mx-auto rounded-xl bg-gray-950 hover:shadow-indigo-900 overflow-hidden hover:shadow-lg ease-linear duration-200 md:h-auto md:w-96"
     >
@@ -17,21 +18,27 @@ const Rooms = ({ data, current }) => {
           <img
             className="w-full h-full object-cover"
             src={
-              current?.selectedVideo?.snippet?.thumbnails?.high?.url || "/1.jpg"
+              checkCurrentIsNull?.selectedVideo?.snippet?.thumbnails?.high
+                ?.url || "/1.jpg"
             }
-            alt={data.room_name || "Room image"}
+            alt={checkDataIsNull?.room_name || "Room image"}
           />
         </div>
         <div className="p-8 flex flex-col justify-center gap-2">
           <label className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-            {data.room_name || ""}
+            {checkDataIsNull?.room_name || "Room Name"}
           </label>
-          <label className="mt-1 text-white">{data.room_desc || ""}</label>
+          <label className="mt-1 text-white">
+            {checkDataIsNull?.room_desc || "Room Description"}
+          </label>
           <label className="block mt-1 text-lg leading-tight font-medium text-white">
-            <TextFormatter title={current?.selectedVideo.snippet.title || ""} />
+            <TextFormatter
+              title={checkCurrentIsNull?.selectedVideo.snippet.title || "Title"}
+            />
           </label>
           <label className="mt-2 text-slate-500">
-            {current?.selectedVideo.snippet.description || ""}
+            {checkCurrentIsNull?.selectedVideo.snippet.description ||
+              "Description"}
           </label>
         </div>
       </div>
