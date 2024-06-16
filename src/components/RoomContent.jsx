be3a3fdd098345a2badd2b7691d4c37314d48fc4
Soliken <kennethrex456@gmail.com>
2024-06-17 00:04:29 +0800

    .

--- src/components/RoomContent.jsx
+++ src/components/RoomContent.jsx
@@ -1,10 +1,12 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import Rooms from "./Rooms";
 import { db } from "../configs/firebaseConfig";
 import { collection, getDocs } from "@firebase/firestore";
 import Loading from "./Loading";
+import dynamic from "next/dynamic";
+
+const Rooms = dynamic(() => import("../components/Rooms"), { ssr: false });
 
 const RoomContent = () => {
   const [roomList, setRoomList] = useState([]);
