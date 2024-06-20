7119f484810e2f276c50eb8fc85dcc37806c3952
Soliken <kennethrex456@gmail.com>
2024-06-20 23:51:32 +0800

    Changing Iframe to ReactPlayer for Dynamicness and other Fixes

--- src/app/rooms/[roomId]/page.jsx
+++ src/app/rooms/[roomId]/page.jsx
@@ -16,29 +16,7 @@ import TextFormatter from "@/src/utils/TextFormatter";
 import Banner from "@/src/components/Banner";
 import Inputbox from "@/src/components/Inputbox";
 import youtubeAPI from "@/src/utils/youtube";
-
-const tailwindColors = [
-  "text-red-500",
-  "text-blue-500",
-  "text-green-500",
-  "text-yellow-500",
-  "text-purple-500",
-  "text-pink-500",
-  "text-indigo-500",
-  "text-teal-500",
-  "text-orange-500",
-  "text-lime-500",
-  "text-cyan-500",
-  "text-amber-500",
-  "text-emerald-500",
-  "text-fuchsia-500",
-  "text-rose-500",
-];
-
-const getRandomColorClass = () => {
-  const randomIndex = Math.floor(Math.random() * tailwindColors.length);
-  return tailwindColors[randomIndex];
-};
+import ReactPlayer from "react-player";
 
 const Room = ({ params }) => {
   const [selectedVideo, setSelectedVideo] = useState(null);
@@ -47,7 +25,6 @@ const Room = ({ params }) => {
   const [session, setSession] = useState([]);
   const [message, setMessage] = useState("");
   const [userData, setUserData] = useState(null);
-  const [userColors, setUserColors] = useState({});
 
   const playingVideo = selectedVideo?.id.videoId || "";
   const roomId = params?.roomId || "";
@@ -107,22 +84,19 @@ const Room = ({ params }) => {
     const sessionCollectionRef = collection(db, "session");
     const q = query(sessionCollectionRef, where("room_id", "==", roomId));
     const unsubscribe = onSnapshot(q, (snapshot) => {
-      const messages = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setSession(messages);
-
-      const newUserColors = { ...userColors };
-      messages.forEach((msg) => {
-        if (!newUserColors[msg.sender_user]) {
-          newUserColors[msg.sender_user] = getRandomColorClass();
-        }
+      const messages = snapshot.docs.map((doc) => {
+        const data = doc.data();
+        const username = data.sender_user.split(" ")[0];
+        return {
+          id: doc.id,
+          ...data,
+          sender_user: username,
+        };
       });
-      setUserColors(newUserColors);
+      setSession(messages);
     });
     return () => unsubscribe();
-  }, [roomId, userColors]);
+  }, [roomId]);
 
   const onCreateMessage = async () => {
     try {
@@ -161,18 +135,24 @@ const Room = ({ params }) => {
         </div>
 
         <div className="w-full h-full flex justify-center items-center p-5 mt-5 md:mt-0">
-          <div className="flex flex-row flex-wrap gap-5 justify-between items-center md:w-full md:h-3/4 md:flex-nowrap">
-            <div className="flex flex-col h-96 w-full md:h-full md:w-full">
-              <iframe
-                className="h-full w-full md:h-full md:w-full rounded-xl"
-                src={`https://www.youtube.com/embed/${playingVideo}?autoplay=1`}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
+          <div className="flex flex-row flex-wrap gap-5 justify-between items-center md:w-full md:h-full md:flex-nowrap">
+            <div className="flex flex-col h-96 w-full p-1 bg-black rounded-xl md:h-full md:w-full">
+              <ReactPlayer
+                style={{ backgroundColor: "transparent" }}
+                width="100%"
+                height="100%"
+                url={`https://www.youtube.com/embed/${playingVideo}?autoplay=1`}
+                playing
+                controls
+              ></ReactPlayer>
             </div>
 
             <div className="flex flex-col gap-2 h-96 w-full md:h-full md:w-2/6 p-5 rounded-xl bg-gray-950">
-              <h2 className="text-lg font-medium text-white">Queued Videos</h2>
+              <div className="flex justify-center items-center">
+                <h2 className="text-lg font-medium text-white">
+                  Queued Videos
+                </h2>
+              </div>
             </div>
           </div>
         </div>
@@ -201,37 +181,35 @@ const Room = ({ params }) => {
                 </a>
               </label>
             </div>
+
             <div className="flex flex-col overflow-auto gap-5">
               {session
                 .filter((session) => session.sender_created !== null)
                 .sort(
                   (a, b) => a.sender_created.seconds - b.sender_created.seconds
                 )
-                .map((session) => {
-                  const colorClass =
-                    userColors[session.sender_user] || getRandomColorClass();
-                  return (
-                    <div key={session.id} className="flex flex-row gap-2">
-                      <label className="text-zinc-500 text-xs">
-                        {session.sender_created
-                          ? new Date(
-                              session.sender_created.seconds * 1000
-                            ).toLocaleTimeString([], {
-                              hour: "2-digit",
-                              minute: "2-digit",
-                            })
-                          : ""}
-                      </label>
-                      <label className={`${colorClass} text-xs`}>
-                        {session.sender_user + ": "}
-                      </label>
-                      <label className="text-white text-xs w-48 md:w-10/12">
-                        {session.sender_message}
-                      </label>
-                    </div>
-                  );
-                })}
+                .map((session) => (
+                  <div key={session.id} className="flex flex-row gap-2">
+                    <label className="text-zinc-500 text-xs">
+                      {session.sender_created
+                        ? new Date(
+                            session.sender_created.seconds * 1000
+                          ).toLocaleTimeString([], {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          })
+                        : ""}
+                    </label>
+                    <label className="text-indigo-400 text-xs">
+                      {session.sender_user + ": "}
+                    </label>
+                    <label className="text-white text-xs w-48 md:w-10/12">
+                      {session.sender_message}
+                    </label>
+                  </div>
+                ))}
             </div>
+
             <div>
               <form onSubmit={onSubmitMessage}>
                 <Inputbox
@@ -245,11 +223,16 @@ const Room = ({ params }) => {
             </div>
           </div>
 
-          <div className="flex flex-col gap-2 md:w-3/6 me-5 pb-3">
+          <div className="flex flex-col gap-2 md:w-3/6 me-5 rounded-xl p-3 bg-gray-950">
+            <div className="flex items-center justify-center h-12">
+              <label className="text-white font-medium text-lg">
+                Video Results
+              </label>
+            </div>
             {searchResults.map((video) => (
               <div
                 key={video.id.videoId}
-                className="w-full h-full md:h-32 rounded-xl flex flex-col md:flex-row gap-5 [&_*]:hover:cursor-pointer ease-linear duration-200 hover:bg-gray-950"
+                className="w-full h-full md:h-32 rounded-xl flex flex-col md:flex-row gap-5 [&_*]:hover:cursor-pointer ease-linear duration-200 hover:bg-gray-900"
                 onClick={() => handleVideoSelect(video)}
               >
                 <img
